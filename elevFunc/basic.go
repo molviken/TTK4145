8c11fa1e75cd54d45a2fc204fcefcd978da56292
molvik <oystein.m@hotmail.com>
2018-02-20 20:42:57 +0100

    lagt til noen elevfunc funksjoner

---
+++ elevFunc/basic.go
@@ -0,0 +1,15 @@
+package elevFunc
+
+import (
+	elevio ".././elevio"
+	"fmt"
+)
+
+
+
+
+func GetOrder(button elevio.ButtonEvent){
+
+	//a := button
+	fmt.Println("Ordered floor: %v\n",button)
+}
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	//"flag"
 	//"time"
 	"fmt"
+	elevFunc "./elevFunc"
 	//bcast "./Network/network/bcast"
 	//localip "./network/localip"
 	elevio "./elevio"
@@ -22,29 +23,33 @@ const (
 	PORT_ELEV = ""
 )
 var simHost string = "15657"
+
+
 func main(){
-	//ip,_ := localip.LocalIP();
-	//fmt.Println(ip)
 	elevio.Init("localhost:15657", 4)
 	fmt.Println("Testing testing")
 	conn.DialBroadcastUDP(15657)
-	elevio.SetDoorOpenLamp(true)
-	c_floor := make(chan int)
-	elevio.SetMotorDirection(elevio.MD_Down)
-	go elevio.PollFloorSensor(c_floor)
-	for{
-		select{
-		case a := <- c_floor:
-			fmt.Println("%+v\n", a)
-			if a == 3{
-				elevio.SetMotorDirection(elevio.MD_Down)	
-			} else if a == 0{
-				elevio.SetMotorDirection(elevio.MD_Up)
-			}
 
+	drv_buttons := make(chan elevio.ButtonEvent)
+	drv_floors  := make(chan int)
+	drv_obstr   := make(chan bool)
+	drv_stop    := make(chan bool) 
+	go elevio.PollButtons(drv_buttons)
+	go elevio.PollFloorSensor(drv_floors)
+	go elevio.PollObstructionSwitch(drv_obstr)
+	go elevio.PollStopButton(drv_stop)
+	
+
+
+	for{
+		
+		select{
+		case a:= <- drv_buttons:
+			elevFunc.GetOrder(a)	
+		case a := <- drv_floors:
+			fmt.Println("Floor sensor:%v\n", a)
 		}
 		
-	}
-	
+	}	
 
 }
